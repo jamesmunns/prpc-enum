b9a5c7539a862d084f1e9b5dc01d2ee1ab7a06c2
James Munns <james@onevariable.com>
2025-08-28 20:16:05 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,123 @@
+// #[derive(Serialize, Deserialize, PartialEq, Schema, Debug, Copy, Clone)]
+// pub enum FilterMode {
+//     // HACK: Plain enum seems to cause issues with postcard/postcard-rpc (send\_resp from client -> server hangs)
+//     Lowpass(u8),
+//     Bandpass(u8),
+// }
+
+use postcard_rpc::{
+    define_dispatch, endpoints,
+    header::{VarHeader, VarSeqKind},
+    host_client::test_channels as client,
+    server::{
+        Dispatch, SpawnContext,
+        impls::test_channels::{
+            ChannelWireRx, ChannelWireSpawn, ChannelWireTx,
+            dispatch_impl::{Settings, WireSpawnImpl, WireTxImpl, new_server},
+        },
+    },
+    topics,
+};
+
+use postcard_schema::Schema;
+use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc;
+
+#[derive(Serialize, Deserialize, PartialEq, Schema, Debug, Copy, Clone)]
+pub enum FilterMode {
+    // HACK: Plain enum seems to cause issues with postcard/postcard-rpc (send\_resp from client -> server hangs)
+    Lowpass,
+    Bandpass,
+}
+
+#[tokio::main]
+async fn main() {
+    let (client_tx, server_rx) = mpsc::channel(16);
+    let (server_tx, client_rx) = mpsc::channel(16);
+
+    let app = SingleDispatcher::new(TestContext {}, ChannelWireSpawn {});
+
+    let cwrx = ChannelWireRx::new(server_rx);
+    let cwtx = ChannelWireTx::new(server_tx);
+    let kkind = app.min_key_len();
+    let mut server = new_server(
+        app,
+        Settings {
+            tx: cwtx,
+            rx: cwrx,
+            buf: 1024,
+            kkind,
+        },
+    );
+    tokio::task::spawn(async move {
+        server.run().await;
+    });
+    let cli = client::new_from_channels(client_tx, client_rx, VarSeqKind::Seq4);
+    for _ in 0..4 {
+        cli.send_resp::<FilterModeEndpoint>(&FilterMode::Lowpass).await.unwrap();
+        cli.send_resp::<FilterModeEndpoint>(&FilterMode::Bandpass).await.unwrap();
+    }
+    println!(":)");
+}
+
+endpoints! {
+    list = ENDPOINT_LIST;
+    | EndpointTy            | RequestTy             | ResponseTy            | Path              | Cfg                    |
+    | ----------            | ---------             | ----------            | ----              | ---                    |
+    | FilterModeEndpoint    | FilterMode            | ()                    | "filter/mode"     |
+}
+
+topics! {
+    list = TOPICS_IN_LIST;
+    direction = postcard_rpc::TopicDirection::ToServer;
+    | TopicTy       | MessageTy             | Path      | Cfg                           |
+    | ----------    | ---------             | ----      | ---                           |
+}
+
+topics! {
+    list = TOPICS_OUT_LIST;
+    direction = postcard_rpc::TopicDirection::ToClient;
+    | TopicTy           | MessageTy     | Path              |
+    | ----------        | ---------     | ----              |
+}
+
+pub struct TestContext {}
+
+pub struct TestSpawnContext {}
+
+impl SpawnContext for TestContext {
+    type SpawnCtxt = TestSpawnContext;
+
+    fn spawn_ctxt(&mut self) -> Self::SpawnCtxt {
+        TestSpawnContext {}
+    }
+}
+
+define_dispatch! {
+    app: SingleDispatcher;
+    spawn_fn: spawn_fn;
+    tx_impl: WireTxImpl;
+    spawn_impl: WireSpawnImpl;
+    context: TestContext;
+
+    endpoints: {
+        list: crate::ENDPOINT_LIST;
+
+        | EndpointTy            | kind          | handler                   |
+        | ----------            | ----          | -------                   |
+        | FilterModeEndpoint    | async         | filter_hdlr               |
+    };
+    topics_in: {
+        list: crate::TOPICS_IN_LIST;
+
+        | TopicTy           | kind      | handler               |
+        | ----------        | ----      | -------               |
+    };
+    topics_out: {
+        list: TOPICS_OUT_LIST;
+    };
+}
+
+async fn filter_hdlr(_context: &mut TestContext, _header: VarHeader, _body: FilterMode) {
+    println!("yey");
+}
